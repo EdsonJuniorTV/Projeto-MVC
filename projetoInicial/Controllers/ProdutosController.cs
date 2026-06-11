d406b2e245a606793fe5f60a88b48cbd4094cf4c
EdsonJuniorTV <edsongustavodiasjunior@gmail.com>
2026-06-11 14:38:08 -0300

    Avanço no Produto

--- projetoInicial/Controllers/ProdutosController.cs
+++ projetoInicial/Controllers/ProdutosController.cs
@@ -8,11 +8,28 @@ namespace ProjetoInicial.Controllers
         private List<Produto> produtos = new List<Produto>()
         {
             new Produto(1, "Notebook", 3000),
-            new Produto(2, "Mouse", 200)
+            new Produto(2, "Mouse", 200),
+            new Produto(3, "Teclado", 800),
+            new Produto(4, "Gabinete", 500),
+            new Produto(5, "Celular", 1000)
         };
         public IActionResult Index()
         {
+            return View(produtos);
+        }
+
+        public IActionResult Criar()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Produto produto)
+        {
+            produto.Id = produtos.Count + 1;
+            produtos.Add(produto);
+
+            return RedirectToAction("Index");
         }
     }
 }
